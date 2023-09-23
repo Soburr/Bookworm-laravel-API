254e9ffa71748a0e5b4040e5d62f118bf43adc1a
Soburr <adebesinbenzenejr99@gmail.com>
2023-09-23 11:16:41 +0100

    feature/authors

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth.api')->prefix('v1')->group(function() {
+Route::middleware('auth:api')->prefix('v1')->group(function() {
     Route::get('/user', function(Request $request) {
         return $request->user();
     });
